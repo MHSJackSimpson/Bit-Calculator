de444e9c8c740f224d2839a5ec706a94186eca98
MHSJackSimpson <simpsonj3018@masseyhigh.school.nz>
2024-05-31 13:11:39 +1200

    Delete C_01_Staement_Generator.py

--- C_01_Staement_Generator.py
+++
@@ -1,8 +0,0 @@
-# functions of here 1 usage
-def statement_generator(statement, decoration):
-    print(f"\n{decoration * 5} {statement} {decoration * 5}")
-
-
-# main routine goes here
-statement_generator(statement = "Instructions", decoration = "-")
-
