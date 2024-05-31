773df8b22f921c0be4dbd8e90dd4db29b4955953
MHSJackSimpson <simpsonj3018@masseyhigh.school.nz>
2024-05-31 13:13:03 +1200

    Working Statement Generator

---
+++ C_01_Staement_Generator.py
@@ -0,0 +1,8 @@
+# functions of here 1 usage
+def statement_generator(statement, decoration):
+    print(f"\n{decoration * 5} {statement} {decoration * 5}")
+
+
+# main routine goes here
+statement_generator(statement = "Instructions", decoration = "-")
+
